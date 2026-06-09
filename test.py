a115cecfd9e26dc9a63afa5f389839f155d9c985
KioshiEagle <jeancharles.vente@gmail.com>
2026-06-09 23:42:30 +0200

    Première pipeline CI/CD

--- test.py
+++
@@ -1,4 +0,0 @@
-import chromadb
-client = chromadb.PersistentClient(path="./chroma_db")
-collection = client.get_collection(name="documents")
-print(f"Nombre d'éléments dans la base : {collection.count()}")
