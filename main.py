43edcb2286a46b82ef0378e0bf4076a52b9483e4
Tobias Nobile <tobias.nobile@telecomnancy.eu>
2026-06-18 16:45:11 +0200

    Acclereated retrievak Qdrant : vector quantization, reduced top k, QDrant client not instancied at each request anymore, lighter embedding model, lighter qwen

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 
 from flask import Flask
 
+from app.extensions import limiter
 from app.routes import bp
 
 app = Flask(
@@ -11,6 +12,7 @@ app = Flask(
 )
 app.secret_key = os.environ.get("FLASK_SECRET_KEY", "change-moi-en-prod")
 
+limiter.init_app(app)
 app.register_blueprint(bp)
 
 if __name__ == "__main__":
